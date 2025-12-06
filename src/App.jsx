3b24bee5b33fc7ddeb56c6c0e4055788c8ee9f7f
TechRahu786 <rock38914@gmail.com>
2025-12-06 11:08:17 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
   const [dayForcast, setdayForcast] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+  console.log("API KEY:", API_KEY);
+if (!API_KEY) {
+  alert("API key missing. Check environment variables.");
+}
+
   const getCurrentWeather = () => {
     navigator.geolocation.getCurrentPosition(
       async (pos) => {
